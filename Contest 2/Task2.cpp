92c6bd9c4749412e87c7ac23b9ede185d440785d
Meliashchenia-MA <mishamel02@gmail.com>
2024-03-11 21:30:53 +0300

    Task 3 added

--- Contest 2/Task2.cpp
+++ Contest 2/Task2.cpp
@@ -4,32 +4,15 @@
 using namespace std;
 
 int main() {
-    int k, n, max = 0, ind = 0;
-    cin >> n >> k;
-    vector <int> a(n);
-    cin >> a[0];
+    int n, x, y;
+    cin >> n;
+    cin >> x >> y;
     if (n == 1){
-        cout << 0;
+        cout << 4;
         return 0;
     }
     for (int i = 1; i < n; i++){
-        cin >> a[i];
+        
     }
-    int i = 0;
-    while(i < n){
-        while(i - ind < k){
-            i++;
-            if(i >= n)
-                break;
-            if (a[i] - a[ind] > max){
-                max = a[i] - a[ind];
-            }
-        }
-        ind++;
-        if (ind >= n)
-            break;
-        i = ind;
-    }
-    cout << max;
     return 0;
 }
